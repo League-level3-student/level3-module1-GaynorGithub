5c63423a4e1c7fe6c6492a7bc1f358bde5999fed
League Student <league@iMac-13.attlocal.net>
2019-11-18 18:27:51 -0800

    IpodShuffle, IntroToStack (IP)

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -1,16 +1,35 @@
 package _01_IntroToArrayLists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 //Copyright The League of Amazing Programmers, 2015
 
-public class _06_IPodShuffle{
+public class _06_IPodShuffle implements ActionListener{
+	JFrame frame;
+	JPanel panel;
+	JButton surprise;
+	
+	
+	Song demo = new Song("demo.mp3");
+	Song harry = new Song("harrystyles.mp3");
+	Song lights = new Song("lights.mp3");
+	Song rodeo = new Song("rodeo.mp3");
+	
+	Random rand = new Random();
+	
+	ArrayList<Song> songs = new ArrayList<Song>();
+	
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-		Song song = new Song("demo.mp3");
-		song.stop();
+		
 				
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
@@ -23,6 +42,38 @@ public class _06_IPodShuffle{
 	}
 	
 	public static void main(String[] args) {
-		new _06_IPodShuffle();
+		
+		_06_IPodShuffle joe = new _06_IPodShuffle();
+		joe.itLives();
 	}
+	
+	public void itLives() {
+		songs.add(demo);
+		songs.add(harry);
+		songs.add(lights);
+		songs.add(rodeo);
+		frame = new JFrame();
+		panel = new JPanel();
+		surprise = new JButton("Surprise Me!");
+		frame.add(panel);
+		panel.add(surprise);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		surprise.addActionListener(this);
+		frame.pack();
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < songs.size(); i++) {
+			songs.get(i).stop();
+		}
+		if(e.getSource() == surprise) {
+			int a = rand.nextInt(4);
+			songs.get(a).play();
+		}
+	}
+	
 }
--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -10,13 +10,29 @@ public class _01_IntroToStack {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
 		
+		Stack<Double> doubles = new Stack();
+		Random rand = new Random();
+		
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		for (int i = 0; i < 100; i++) {
+			double j = (rand.nextDouble()*100);
+			doubles.push(j);
+		}
 		
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		String num1 = JOptionPane.showInputDialog(null, "Please enter a number between 0 and 100");
+		double one = Double.parseDouble(num1);
+		String num2 = JOptionPane.showInputDialog(null, "Please enter a number between 0 and 100");
+		double two = Double.parseDouble(num2);
 		
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		for (int i = 0; i < doubles.size(); i++) {
+			Double k = doubles.pop();
+			if((k < one && k > two) || (k < two && k > one)) {
+				System.out.println(k);
+			}
+		}
 		
 		//   EXAMPLE:
 		//   NUM 1: 65
