0d46f32830d2c1b39dfc68d12b0bd94a3788840c
League Student <league@iMac-11.attlocal.net>
2019-12-16 18:25:36 -0800

    HangMan (IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,52 @@
 package _04_HangMan;
 
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class HangMan{
 	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	Utilities u = new Utilities();
+	Stack<String> words = new Stack<String>();
 	
+	
+	public static void main(String[] args) {
+		HangMan h = new HangMan();
+		h.hang();
+		
+	}
+	
+	void hang() {
+		
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of words you want to solve."));
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		frame.add(label);
+		for (int i = 0; i < num; i++) {
+			String y = Utilities.readRandomLineFromFile("dictionary.txt");
+			if (words.contains(y)) {
+				i--;
+			} else {
+				words.push(y);
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
 	
 }
