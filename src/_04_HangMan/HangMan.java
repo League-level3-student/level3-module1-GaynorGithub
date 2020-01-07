47a53e5a0b6813aa2f2d481c56695415669660db
League Student <league@iMac-11.attlocal.net>
2020-01-06 18:26:35 -0800

    HangMan (IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,19 +1,27 @@
 package _04_HangMan;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.lang.reflect.Array;
 import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
 
-public class HangMan{
+public class HangMan implements KeyListener{
 	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	String word = "";
 	Utilities u = new Utilities();
 	Stack<String> words = new Stack<String>();
+	char[] chars;
+	char[] rchars;
+	
 	
 	
 	public static void main(String[] args) {
@@ -28,7 +36,9 @@ public class HangMan{
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
-		frame.add(label);
+		panel.add(label);
+		frame.addKeyListener(this);
+		
 		for (int i = 0; i < num; i++) {
 			String y = Utilities.readRandomLineFromFile("dictionary.txt");
 			if (words.contains(y)) {
@@ -36,16 +46,69 @@ public class HangMan{
 			} else {
 				words.push(y);
 			}
-			
-			
-			
+		}
+		
+		word = popWord();
+		chars = new char[word.length()];
+		rchars = new char[word.length()];
+		for (int i = 0; i < word.length(); i++) {
+			chars[i] = word.charAt(i);
+			rchars[i] = '_';
 		}
 		
 		
 		
+		frame.pack();
 		
 		
+	}
+	
+	String popWord() {
+		String y = "";
+		if(!words.isEmpty()) {
+			y = words.pop();
+			System.out.println(y);
+			System.out.println(y.length());
+			String d = "";
+			for (int j = 0; j < y.length(); j++) {
+				d=d+"_";
+				label.setText(d);
+			}
+		}
 		
+		
+		return y;
+	}
+	
+	String makeStringFrom(char[] r) {
+		String g = "";
+		for (int i = 0; i < r.length; i++) {
+			g = g + r[i];
+		}
+		
+		return g;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < word.length(); i++) {
+			if(e.getKeyChar() == word.charAt(i)) {
+				rchars[i] = chars[i];
+				label.setText(makeStringFrom(rchars));
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 	
